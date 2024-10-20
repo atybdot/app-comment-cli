d312a1a26138e791ca56c42467f8697b91b98a37
atybdot <180611249+atybdot@users.noreply.github.com>
2024-10-21 01:07:25 +0530

    initial commit

--- appComment.config.js
+++
@@ -1,9 +0,0 @@
-const appCommentConfig = {
-    url : import.meta.env.VITE_APPCOMMENT_BASE_URL,
-    projectId : import.meta.env.VITE_APPCOMMENT_PROJECT_ID,
-    api : import.meta.env.VITE_APPCOMMENT_API,
-    databaseId : import.meta.env.VITE_APPCOMMENT_DATABASE_ID,
-    collectionId : import.meta.env.VITE_APPCOMMENT_COLLECTION_ID,
-};
-
-export default appCommentConfig
