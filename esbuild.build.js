3e10ee6e9985ef5092da3c3a630c7157677c598c
atybdot <180611249+atybdot@users.noreply.github.com>
2024-10-21 13:46:07 +0530

    updated esbuild

--- esbuild.build.js
+++ esbuild.build.js
@@ -3,12 +3,13 @@ import esbuild from 'esbuild';
 esbuild.build({
     entryPoints : ["./src/**/*"],
     outdir : "dist/",
-    outbase : "src/",
     format : "esm",
     platform : "node",
     treeShaking : true,
     minify : true,
     splitting : true,
     loader : { '.sample' : "text"},
+    packages : "bundle",
+    sourcemap : "external" 
     
 })
